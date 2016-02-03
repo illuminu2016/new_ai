f0363ef2c484d1d199a1b9a09f33a09f91f5269e
Constantin Crismaru <illuminu2008@yahoo.com>
2016-02-04 00:20:51 +0200

    login page and profile page

---
+++ scripts/login.js
@@ -0,0 +1,8 @@
+function logInApp() {
+	$('.uv_799-inner-container').hide();
+	$('.uv_789-inner-container').show();
+}
+function singUpApp() {
+	$('.uv_789-inner-container').hide();
+	$('.uv_799-inner-container').show();
+}
