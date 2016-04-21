84a5271a85823d6172701c030ca4ce4f4c7fd137
Constantin Crismaru <illuminu2008@yahoo.com>
2016-04-21 19:09:50 +0300

    search_profile popovers

--- scripts/menu_functions.js
+++ scripts/menu_functions.js
@@ -2,20 +2,32 @@ var searchUsr = false,
 	emailUsr = false,
 	uploadPhoto = false,
 	galleryUsr =  false,
+	messageUsr = false,
+	sharePost = false,
+	aiSharePost = false,
+	reportPost = false,
 	selfAlbum = {
 		position: 0,
 		cards: 0
+	},
+	profileMap = {
+		openStatus: false,
+		name: ''
 	};
 
 $('*').click(function(e){
 	searchUsr = targetSearchEvt(e, searchUsr, '._hm-typeahead-856', '._hm-search-978', '._hm-search-user-overflow', '._hm-search-inner-overflow');
 	emailUsr = targetEvt(e, emailUsr, '.fa-envelope56', '._hm-email-user-overflow', '._hm-email-inner-overflow');
+	messageUsr = targetEvt(e, messageUsr, '.multiglass-884', '._message-ai-83', '._inner-message-ai-83');
+	sharePost = targetShareEvt(e, sharePost, '.fa-share-alt', '._mds933', '.fa-times');
+	reportPost = targetShareEvt(e, reportPost, '.fa-flag', '._mds182', '.fa-times');
+	aiSharePost = targetShareEvt(e, aiSharePost, '.fa-bolt', '._mds939', '.fa-times');
 	galleryUsr = targetGalleryEvt(e, galleryUsr, '._ijl-874', '._mds932', '._unk877', '._imgk984');
 	uploadPhoto = targetUploadEvt(e, uploadPhoto, '.vw-album998', '._upload-overlay899');
 });
 
 function initMap() {
-	var mapDiv = document.getElementById('map');
+	var mapDiv = document.getElementById('map_placeholder');
 	var map = new google.maps.Map(mapDiv, {
 		center: {lat: 47.15, lng: 27.59},
 		zoom: 12
@@ -33,6 +45,28 @@ function targetUploadEvt(event, target, btn, container) {
 		if($(event.target).is(container)) {
 			if(target) {
 				target = !target;
+				$('html').css('overflow', 'scroll');
+			}
+		}
+	}
+
+	target ? $(container).show() : $(container).hide();
+
+	return target;
+}
+
+function targetShareEvt(event, target, btn, container, closeBtn) {
+	if($(event.target).is(btn)) {
+		target = !target;
+
+		$('html').css('overflow', 'hidden');
+
+		event.stopPropagation();
+	} else {
+		if($(event.target).is(container) || $(event.target).is(closeBtn)) {
+			if(target) {
+				target = !target;
+				$('html').css('overflow', 'scroll');
 			}
 		}
 	}
@@ -185,5 +219,248 @@ function preparePostTimb() {
 }
 
 function activateLocation() {
-	$('#ai-main-99').css('color', '#286090');
+	$('#ai-main-99').css('color', '#5cb85c');
 }
+
+function editProfileInformation() {
+	$('._profile-edit-88').each(function(){
+		$(this).attr('contenteditable', true);
+		$(this).css('background', '#f7f7f7');
+	})
+	$('#save-profile-info').show();
+	profileMap.openStatus = true;
+}
+
+function saveProfileInformation() {
+	$('#save-profile-info').hide();
+	$('#pencil-profile-info').hide();
+	$('#_profile-information-loader').fadeIn(1000,"linear");
+	profileMap.openStatus = false;
+	setTimeout(function(){
+		$('._profile-edit-88').each(function(){
+			$(this).attr('contenteditable', false);
+			$(this).css('background', 'white');
+		});
+		$('#_profile-information-loader').hide();
+		$('#pencil-profile-info').fadeIn(1000,"linear");
+	}, 1000);
+}
+
+function editInterests() {
+	$('#save-profile-interest').show();
+	$('.tokenfield').fadeIn(1000,"linear");
+	$('._hobby-close-67').each(function(){
+		$(this).show();
+	});
+}
+
+function saveInterests() {
+	$('#save-profile-interest').hide();
+	$('#pencil-profile-interest').hide();
+	$('#_profile-interest-loader').fadeIn(1000,"linear");
+	setTimeout(function(){
+		$('#_profile-interest-loader').hide();
+		$('#pencil-profile-interest').fadeIn(1000,"linear");
+		$('.tokenfield').fadeOut(1000,"linear", function(){
+			$('._hobby-close-67').each(function(){
+				$(this).hide();
+			});
+		});
+	}, 1000);
+}
+
+function removeInterest(id) {
+	$('#'+id).hide();
+}
+
+function editProfileMoreInformation(btn1, btn2, btn3, selectOpt) {
+	$('#' + btn1).hide();
+	$('#' + btn2).show();
+	$('#' + btn3).hide();
+	$('#' + selectOpt).show();
+}
+
+function saveProfileMoreInformation(btn1, btn2, btn3, loader, selectOpt) {
+	$('#' + btn1).hide();
+	$('#' + loader).fadeIn(1000,"linear");
+	setTimeout(function(){
+		$('#' + loader).hide();
+		$('#' + btn2).show();
+		$('#' + selectOpt).hide();
+		$('#' + btn3).show();
+	}, 1000);
+
+}
+
+function openMapSelection() {
+	if(profileMap.openStatus) {
+		$('._map-overlay98').css('visibility', 'visible');
+		$('._map-overlay98').css('z-index', '100');
+		$('html').css('overflow', 'hidden');
+	}
+}
+
+function saveGlobalPosition() {
+	if(profileMap.name.length > 0) {
+		$('#position-input-map').html(profileMap.name);
+	}
+	hideMapOverlay();
+}
+
+function hideMapOverlay() {
+	$('._map-overlay98').css('visibility', 'hidden');
+	$('._map-overlay98').css('z-index', '1');
+	$('html').css('overflow', 'scroll');
+}
+
+if(document.getElementById("tokenfield")) {
+	$('#tokenfield').tokenfield();
+}
+
+function zoomPulse(container, btn) {
+	$('#' + container).addClass('_zoom-green100');
+	$('#' + btn).addClass('transition');
+	setTimeout(function(){
+		$('#' + btn).removeClass('transition');
+	}, 1000);
+}
+
+function retimb(value) {
+	$('._retimb-btn-506').hide();
+	$('._retimb-loader').fadeIn(1000,"linear");
+	setTimeout(function(){
+		$('._retimb-loader').hide();
+		if(value == 0) {
+			$('._mds933').hide();
+			sharePost = false;
+		} else {
+			$('._mds939').hide();
+			aiSharePost = false;
+		}
+		$('._retimb-btn-506').show();
+		$('html').css('overflow', 'scroll');
+	}, 1000);
+}
+
+function reportPostFn() {
+	$('._report-btn-193').hide();
+	$('._report-loader').fadeIn(1000,"linear");
+	setTimeout(function(){
+		$('._report-loader').hide();
+		$('._mds182').hide();
+		reportPost = false;
+		$('._report-btn-193').show();
+		$('html').css('overflow', 'scroll');
+	}, 1000);
+}
+
+function retimbAddComment() {
+	$('._add-comment89').html('');
+	$('._retimb100').css('min-height', '70px');
+}
+
+$(function() {
+	var element = null;
+	var options = {
+		placement: function (context, source) {
+			var $win, $source, winWidth, popoverWidth, popoverHeight, offset, toRight, toLeft, placement, scrollTop;
+
+			$win = $(window);
+			$source = $(source);
+			element = placement = $source.attr('data-placement');
+			popoverWidth = 400;
+			popoverHeight = 270;
+			offset = $source.offset();
+
+			// Check for horizontal positioning and try to use it.
+			if (placement.match(/^right|left$/)) {
+				winWidth = $win.width();
+				toRight = winWidth - offset.left - source.offsetWidth;
+				toLeft = offset.left;
+
+				if (placement === 'left') {
+					if (toLeft > popoverWidth) {
+						return 'left';
+					}
+					else if (toRight > popoverWidth) {
+						return 'right';
+					}
+				}
+				else {
+					if (toRight > popoverWidth) {
+						return 'right';
+					}
+					else if (toLeft > popoverWidth) {
+						return 'left';
+					}
+				}
+			}
+
+			// Handle vertical positioning.
+			scrollTop = $win.scrollTop();
+			if (placement === 'bottom') {
+				if (($win.height() + scrollTop) - (offset.top + source.offsetHeight) > popoverHeight) {
+					return 'bottom';
+				}
+				return 'top';
+			}
+			else {
+				if (offset.top - scrollTop > popoverHeight) {
+					return 'top';
+				}
+				return 'bottom';
+			}
+		},
+		trigger: 'manual',
+		html: true,
+		content: function() {
+			return $('#' + ($(this).attr('data-user'))).html();
+		},
+		animation: false
+	};
+
+	$('._popover301').popover(options)
+		.on("mouseenter", function () {
+			var _this = this;
+			$(this).popover("show");
+			$(".popover").on("mouseleave", function () {
+				$(_this).popover('hide');
+			});
+		}).on("mouseleave", function () {
+			var _this = this;
+			setTimeout(function () {
+				if (!$(".popover:hover").length) {
+					$(_this).popover("hide");
+					popOverState = false;
+				}
+			}, 300);
+		});
+
+
+	if($('._report-overlay').html()) {
+		$('._report-overlay').draggable({cursor: "move"});
+	}
+	if($('._retimb-container933').html()) {
+		$('._retimb-container933').draggable({cursor: "move"});
+	}
+	$('.fa-share-alt').tooltip();
+	$('.fa-heart').tooltip();
+	$('.fa-bolt').tooltip();
+	$('.fa-flag').tooltip();
+/*	$(document).tooltip({
+		track: true,
+		items: "[data-profile]",
+		content: function() {
+			var element = $( this );
+			if ( element.is( "[data-profile]" ) ) {
+				return "";
+			}
+		}
+	});*/
+});
+
+function addContact(source) {
+	$(source).html('Contact requested');
+};
+
+
