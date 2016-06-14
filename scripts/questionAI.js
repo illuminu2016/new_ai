24a1a53b8af319203417555383feefd92791e55a
Constantin Crismaru <illuminu2008@yahoo.com>
2016-06-15 01:05:20 +0300

    sign up new account

---
+++ scripts/questionAI.js
@@ -0,0 +1,9 @@
+$('._ai-often-822 input').prop('disabled', true);
+$('._ai-often-823 input').prop('disabled', true);
+
+setTimeout(function(){
+    $('.controls-loader').hide();
+    $('.illustration-graph-38').fadeIn(1000,"linear");
+    $('.tabular-730').draggable({ containment: "parent" });
+}, 2000);
+
